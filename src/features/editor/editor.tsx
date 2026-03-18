7ec5f3b789591d3f2ba89909dcdeebb26600a8a8
ki8yk8 <ki8yk8@gmail.com>
2026-03-18 10:15:33 +0545

    adds skeleton for the editor popup

--- src/features/editor/editor.tsx
+++ src/features/editor/editor.tsx
@@ -11,11 +11,11 @@ import { expandSelectionToWords, getSelection } from "./utils"
 import {
   Popover,
   PopoverContent,
-  PopoverDescription,
-  PopoverHeader,
-  PopoverTitle,
   PopoverTrigger,
 } from "@/components/ui/popover"
+import { Field, FieldDescription, FieldLabel } from "@/components/ui/field"
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button"
 
 const HEADER_DESCRIPTION = "You can select the span of text to correct it."
 // BUG: at the end of the text there will be always mutliple spaces. This is to have the selection to end because the issue with current one is if selected to end it will not display all the selected.
@@ -44,7 +44,6 @@ export default function Editor({ text = INITIAL_TEXT, onChange }: EditorProps) {
 
     setPopOpen(true)
     setPopPosition([rect.left, rect.bottom + 2])
-    console.log(rect.left)
   }
 
   return (
@@ -57,13 +56,23 @@ export default function Editor({ text = INITIAL_TEXT, onChange }: EditorProps) {
           className="fixed"
           style={{ left: popPosition[0], top: popPosition[1] }}
         >
-          <PopoverHeader>
-            <PopoverTitle>Correction</PopoverTitle>
-            <PopoverDescription>
-              Edits only the content you have selected. If you want to exapnd
-              the correction you need to reselect.
-            </PopoverDescription>
-          </PopoverHeader>
+          <div>
+            <Field>
+              <FieldLabel htmlFor="popover-edit-content">Correction</FieldLabel>
+              <Textarea
+                id="popover-edit-content"
+                placeholder="Corrected Text"
+              />
+              <FieldDescription>
+                Edits only the content you have selected. If you want to exapnd
+                the correction you need to reselect.
+              </FieldDescription>
+            </Field>
+
+            <div className="float-right">
+              <Button>Done</Button>
+            </div>
+          </div>
         </PopoverContent>
       </Popover>
 
