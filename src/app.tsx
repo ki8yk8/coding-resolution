9d1bc83c21556d5fbdd73bddb2084fa5e6a57955
ki8yk8 <ki8yk8@gmail.com>
2026-03-31 23:32:41 +0545

    adds a blockquote

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,7 @@ import Editor from "./features/editor/editor"
 
 export function App() {
   return (
-    <div>
+    <div className="flex h-screen flex-col gap-4">
       <main className="flex items-center justify-between gap-14 px-10 py-5">
         <div>
           <h1 className="text-2xl font-bold text-green-400">
@@ -20,7 +20,23 @@ export function App() {
         </div>
       </main>
 
-      <footer></footer>
+      <div className="flex justify-center pt-9">
+        <div className="relative w-2/3">
+          <p className="absolute -top-4 left-4 text-8xl font-extrabold opacity-10">
+            "
+          </p>
+          <p className="px-10 text-center text-4xl font-extralight">
+            Once a wise man said, A quick brown fox quickly jumped over the lazy
+            dog.
+          </p>
+        </div>
+      </div>
+
+      <footer className="flex grow items-end justify-center py-4">
+        <p className="text-center text-sm">
+          Made by KI8YK8. Thanks to resolution coding.
+        </p>
+      </footer>
     </div>
   )
 }
